397d45b16c4e4dce2a88574ef11e858eea3fdda2
Alexenia Weibel <alexenia.weibel@gmail.com>
2024-12-10 11:38:27 +0100

    Project initialization

---
+++ gtd/src/main/java/com/mitarashi_dango_codes/gtd/GtdApplication.java
@@ -0,0 +1,13 @@
+package com.mitarashi_dango_codes.gtd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtdApplication.class, args);
+	}
+
+}
